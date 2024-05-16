72843edeae57b0479f5381e06aba9cb5a8c68905
ElectroReel <145266039+ElectroReel@users.noreply.github.com>
2024-05-16 16:16:25 +0200

    Update pong5.py

--- pong5.py
+++ pong5.py
@@ -1,10 +1,9 @@
 import pygame
-from random import randint, random
 
 pygame.init()
 
-WIDTH = ___
-HEIGHT = ___
+WIDTH = 800
+HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption('My Game')
@@ -16,24 +15,24 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 255)
 
-screen.fill(___)
+screen.fill(RED)
 pygame.display.update()
 
-radius = ___
-x = ___
-y = ___
+radius = 10
+x = WIDTH//2
+y = radius + 10
 
-pygame.draw.circle(___)  # Position is the center of the circle.
+pygame.draw.circle(screen, WHITE, (x, y), radius)  # Position is the center of the circle.
 
 
-paddle = { "width" : ___,
-           "height": ___,
-           "color" : ___,
-           "x"     : ___,
-           "y"     : ___}
-paddle["x"] = ___
-paddle["y"] = ___
-pygame.draw.rect(___)
+paddle = { "width" : 200,
+           "height": 20,
+           "color" : BLUE,
+           "x"     : 0,
+           "y"     : HEIGHT}
+paddle["x"] = WIDTH//2 - paddle["width"]//2
+paddle["y"] = HEIGHT - paddle["height"]
+pygame.draw.rect(screen, BLUE, (paddle["x"], paddle["y"], paddle["width"], paddle["height"]))
 
 speed = 5
 x_sens = y_sens = 1
@@ -42,7 +41,7 @@ pause = False
 
 end = False
 while not end:
-    screen.fill(___)
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end = True
@@ -50,10 +49,10 @@ while not end:
     key = pygame.key.get_pressed()
 
     if key[pygame.K_SPACE]:
-        pause = ___
+        pause = True
 
     if key[pygame.K_RETURN]:
-        pause = ___
+        pause = False
 
     if key[pygame.K_m]:
         auto = False
@@ -62,38 +61,65 @@ while not end:
 
         if key[pygame.K_LEFT]:
             print("Key LEFT pressed")
-            ???
+            paddle["x"] = paddle["x"] - speed
 
         if key[pygame.K_RIGHT]:
             print("Key RIGHT pressed")
-            ???
+            paddle["x"] = paddle["x"] + speed
 
         # change x direction if the ball hits the left or right edge
-        ???
+        if x <radius or x > WIDTH - radius:
+            x_sens = -x_sens
 
         # change y direction if the ball hits the top edge
-        ???
+        if y < radius:
+            y_sens = -y_sens
 
-         # if the ball hits the paddle top
-        ???
+        # if the ball hits the paddle top
+        if y >= paddle["y"]-radius:
             # if the ball is between the x paddle begin and the x paddle end
-            ???
+            if x > paddle["x"] and x < paddle["x"]+paddle["width"]:
                 # change y direction
-                ???
+                y_sens = -y_sens
 
         # if the ball comes out of the screen from below, end the game
-        ???
+        if y > HEIGHT:
+            end = True
 
         # compute the new ball coordinates
         x = x + x_sens * speed
         y = y + y_sens * speed
 
-    # redraw ball and paddle
+myfont = pygame.font.SysFont('monospace', 50)
+
+print("pong6")
+screen.fill(BLACK)
+title = myfont.render("Single Player Pong:", False, GREEN)
+screen.blit(title, (WIDTH // 2 - title.get_width() // 2, HEIGHT // 2 - title.get_height() * 2))
+pygame.display.update()
+pygame.time.delay(1000)
+
+# countdown before start game
+# loop from 3 to 0 and write the number in the middle of the screen
+
+
+...
+
+pause = False
+end = False
+while not end:
+    screen.fill(BLACK)
+    # Control the game
+    # Past your code from pong5.py
+
     pygame.draw.circle(screen, WHITE, (x, y), radius)
     pygame.draw.rect(screen, paddle["color"], (paddle["x"], paddle["y"], paddle["width"], paddle["height"]))
 
-    # update screen
+    # Display the score in position (10, 0) (top left on the screen)
+
     pygame.display.update()
     pygame.time.delay(10)
 
+# Wait a bit to be sure the player knows his score
+pygame.time.delay(2000)
 pygame.quit()
